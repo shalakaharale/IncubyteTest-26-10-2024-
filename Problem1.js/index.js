c68116729213973a0a791f03170c7d85096be2b9
shalakaharale <haralesn@gmail.com>
2024-10-26 11:42:21 +0530

    Not allowing negative numbers

--- Problem1.js/index.js
+++ Problem1.js/index.js
@@ -2,9 +2,12 @@ const addNumbers = (numAsString) => {
   const arr = numAsString.split(/['\n,]/);
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
+    if (Number(arr[i]) < 0) {
+      return `negative numbers not allowed ${Number(arr[i])}`;
+    }
     sum = sum + Number(arr[i]);
   }
   return sum;
 };
 
-console.log(addNumbers("1\n3,5,6"));
+console.log(addNumbers("-11\n3,5,6"));
